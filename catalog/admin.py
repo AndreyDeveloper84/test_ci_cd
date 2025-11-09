df7b4861a02b870a89cdc511ca8586e6eac44db0
AndreyDeveloper84 <tikhonovmaksoft@gmail.com>
2025-11-09 13:33:21 +0300

    adding admin.py register Unit

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Order, Unit
 
-admin.site.register(Order)
-admin.site.register(Unit)
+admin.site.register(Order, Unit)
+
 
 # Register your models here.
