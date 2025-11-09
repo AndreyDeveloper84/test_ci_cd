995c691293d99e421ba232132c46b02ac36ffd25
AndreyDeveloper84 <tikhonovmaksoft@gmail.com>
2025-11-09 13:25:29 +0300

    migrate model Unit

--- config/settings/base.py
+++ config/settings/base.py
@@ -22,7 +22,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',       # Сессии
     'django.contrib.messages',       # Сообщения (flash messages)
     'django.contrib.staticfiles',    # Статические файлы (CSS, JS, изображения)
-    'catalog.apps.CatalogConfig',
+    'catalog',
     
     # Здесь будут твои приложения, например:
     # 'blog',
