ecf296fb93ba2018ed009ec6b23dd2365c2c8d36
AndreyDeveloper84 <tikhonovmaksoft@gmail.com>
2025-11-09 14:53:10 +0300

    fixing error Unit register admin

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from .models import Order, Unit
 
-admin.site.register(Order, Unit)
-
+admin.site.register(Order)
+admin.site.register(Unit)
 
 # Register your models here.
