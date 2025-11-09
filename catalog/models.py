72e042812c7c63f2be2844f91cebf0b74b082b27
AndreyDeveloper84 <tikhonovmaksoft@gmail.com>
2025-11-09 12:54:07 +0300

    Adding model Unit

--- catalog/models.py
+++ catalog/models.py
@@ -5,3 +5,9 @@ class Order(models.Model):
 
     def __str__(self):
         return self.title
+
+class Unit(models.Model)
+    title = models.CharField(max_length=250)
+
+    def __str__(self):
+        return self.title 
