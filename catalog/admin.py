6946f0dbc05566fc42997f4aba948354730887e2
AndreyDeveloper84 <tikhonovmaksoft@gmail.com>
2025-11-17 05:19:53 +0300

    Add Unit model to catalog

--- catalog/admin.py
+++ catalog/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Order, Unit
 
 admin.site.register(Order)
-admin.site.register(Unit)
 
-# Register your models here.
+@admin.register(Unit)
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ["title"]
